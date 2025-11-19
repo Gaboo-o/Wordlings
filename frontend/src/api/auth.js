60d56ddfd02286219ed7a53aafe97ee796bf049c
Gabo <gabrielvalle33210@gmail.com>
2025-11-18 16:50:46 -0800

    visual changes

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,23 +1,29 @@
-import axios from 'axios';
-import { csrfHeader } from './_csrf';
+import axios from "axios";
+import { csrfHeader } from "./_csrf";
 
 export const login = async (username, password) => {
-  const res = await axios.post('/api/auth/login', { username, password }, { withCredentials: true, headers: csrfHeader() });
-  return res.data; 
-}
+  const res = await axios.post(
+    "/api/auth/login",
+    { username, password },
+    { withCredentials: true, headers: csrfHeader() }
+  );
+  return res.data;
+};
 
 export const signup = async (username, password) => {
-  const res = await axios.post('/api/auth/signup', { username, password }, { withCredentials: true, headers: csrfHeader() });
+  const res = await axios.post(
+    "/api/auth/signup",
+    { username, password },
+    { withCredentials: true, headers: csrfHeader() }
+  );
   return res.data;
-}
+};
 
 export const logout = async () => {
-  try { 
-    await axios.post('/api/auth/logout', {}, { withCredentials: true, headers: csrfHeader() }); 
-  } 
-  catch {
-
-  }
-
-  return { message: 'logged out' };
+  await axios.post(
+    "/api/auth/logout",
+    {},
+    { withCredentials: true, headers: csrfHeader() }
+  );
+  return { message: "logged out" };
 };
--- frontend/src/api/words.js
+++ frontend/src/api/words.js
@@ -1,23 +1,36 @@
 import axios from "axios";
-import { csrfHeader } from './_csrf';
+import { csrfHeader } from "./_csrf";
 
 export const fetchTrends = async (word) =>
   (await axios.get(`/api/trends/${encodeURIComponent(word)}`)).data;
 
 export const fetchWords = async (params = {}) =>
-  (await axios.get('/api/words', { params })).data;
+  (await axios.get("/api/words", { params })).data;
 
 export const fetchWordById = async (id) =>
   (await axios.get(`/api/words/${id}`)).data;
 
 export const searchWords = async (searchTerm) =>
-  (await axios.get('/api/words/search', { params: { search: searchTerm } })).data;
+  (await axios.get("/api/words/search", { params: { search: searchTerm } }))
+    .data;
 
 export const addWord = async ({ word, definition, examples }) =>
-  (await axios.post('/api/words/add', { word, definition, examples }, { withCredentials: true, headers: csrfHeader() })).data;
+  (
+    await axios.post(
+      "/api/words/add",
+      { word, definition, examples },
+      { withCredentials: true, headers: csrfHeader() }
+    )
+  ).data;
 
 export const upvoteWord = async (id) =>
-  (await axios.post(`/api/words/upvote/${id}`, {}, { withCredentials: true, headers: csrfHeader() })).data;
+  (
+    await axios.post(
+      `/api/words/upvote/${id}`,
+      {},
+      { withCredentials: true, headers: csrfHeader() }
+    )
+  ).data;
 
 export const fetchSimilar = async ({ wordId, word, limit = 12 }) => {
   const params = {};
